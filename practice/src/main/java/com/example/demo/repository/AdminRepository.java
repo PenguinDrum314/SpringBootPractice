7a655093c684bee7ced8e37a97a387ef58f94dbe
PenguinDrum314 <pipipikku@gmail.com>
2026-08-21 13:37:01 +0900

    ログイン ログインボタンクリックしてもお問い合わせ一覧に飛ばない

--- practice/src/main/java/com/example/demo/repository/AdminRepository.java
+++ practice/src/main/java/com/example/demo/repository/AdminRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Admin;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+	// NOTE: Adminのemailが一致する管理者を検索します
+	Optional<Admin> findByEmail(String email);
 
 }
